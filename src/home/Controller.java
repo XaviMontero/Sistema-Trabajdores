7e7ac92d41e67f26ca1d3b38d94ee276dc827c73
Xavi Montero <oxmonteroc83@est.ucacue.edu.ec>
2019-04-14 14:06:06 -0500

    as

--- src/home/Controller.java
+++ src/home/Controller.java
@@ -1,5 +1,6 @@
 package home;
 
+import javafx.embed.swing.SwingNode;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -10,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -42,20 +44,15 @@ public class Controller implements Initializable {
     private Button btnSignout;
 
     @FXML
-    private Pane pnlCustomer;
-
+    private Pane pnRegistro;
     @FXML
-    private Pane pnlOrders;
+    private StackPane deskotpPane;
 
-    @FXML
-    private Pane pnlOverview;
 
-    @FXML
-    private Pane pnlMenus;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Node[] nodes = new Node[10];
+        Node[] nodes = new Node[3];
         for (int i = 0; i < nodes.length; i++) {
             try {
 
@@ -80,22 +77,14 @@ public class Controller implements Initializable {
 
 
     public void handleClicks(ActionEvent actionEvent) {
-        if (actionEvent.getSource() == btnCustomers) {
-            pnlCustomer.setStyle("-fx-background-color : #1620A1");
-            pnlCustomer.toFront();
-        }
-        if (actionEvent.getSource() == btnMenus) {
-            pnlMenus.setStyle("-fx-background-color : #53639F");
-            pnlMenus.toFront();
-        }
         if (actionEvent.getSource() == btnOverview) {
-            pnlOverview.setStyle("-fx-background-color : #02030A");
-            pnlOverview.toFront();
+            pnRegistro.toFront();
+
         }
-        if(actionEvent.getSource()==btnOrders)
-        {
-            pnlOrders.setStyle("-fx-background-color : #464F67");
-            pnlOrders.toFront();
-        }
+
+
+
+
     }
+
 }
